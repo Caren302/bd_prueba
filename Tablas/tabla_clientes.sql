c5220fb793aaefe40adef33d85ed977ac6e0853a
Caren  Rodríguez <97258545+Caren302@users.noreply.github.com>
2022-05-09 14:07:02 +0000

    Hoy vimos lo que son los índices

--- Tablas/tabla_clientes.sql
+++ Tablas/tabla_clientes.sql
@@ -1,12 +1,23 @@
+SELECT 'Creacion de tabla_clientes';
+
 CREATE TABLE clientes (
     id_cliente integer PRIMARY KEY AUTOINCREMENT,
     nombre varchar(50),
     email varchar(50)
 );
 
+SELECT 'Insertar registro en clientes';
+
 INSERT INTO clientes(nombre,email)
 VALUES
 ('Dejah','dejah@email.com'),
 ('Jonh','jonh@email.com');
 
+SELECT 'Cosulta tabla_clientes';
+
 SELECT * FROM clientes;
+
+SELECT 'Creacion de indices';
+
+CREATE UNIQUE INDEX nombre_email ON clientes(nombre,email);
+CREATE UNIQUE INDEX email ON clientes(email);
---
+++ clientes.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes(nombre,email) VALUES ('Dejah','dejah@email.com'),
+('Dejah','otro@email.com'),
+('Otro','dejah@email.com');
+
+DROP INDEX nombre_email;
